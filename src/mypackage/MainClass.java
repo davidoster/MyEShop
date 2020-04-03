d97f75f9adf674311271a76282edc323aebba2af
David Oster <paspa@hotmail.com>
2020-04-03 18:40:06 +0300

    update

--- src/mypackage/MainClass.java
+++ src/mypackage/MainClass.java
@@ -11,6 +11,8 @@ import java.sql.Connection;
 import java.sql.Statement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 
@@ -21,21 +23,39 @@ import java.sql.SQLException;
  */
 public class MainClass {
     private static final String DB_URL = "localhost:3306";
-    private static final String FULL_DB_URL = "jdbc:mysql://" + DB_URL + "/eshop?zeroDateTimeBehavior=convertToNull";
+    private static final String FULL_DB_URL = "jdbc:mysql://" + DB_URL + "/eshop?zeroDateTimeBehavior=CONVERT_TO_NULL&useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC&allowPublicKeyRetrieval=true&useSSL=false";
     private static final String DB_USER = "root";
-    private static final String DB_PASSWD = "root";
+    private static final String DB_PASSWD = "Root1234!";
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Connection connection = null;
-        Statement statement = null;
-        ResultSet resultSet = null;
-
-        Customer customer = new Customer();
-        
-        DriverManager.getConnection(FULL_DB_URL, DB_USER, DB_PASSWD);
+        try {
+            Connection connection = null;
+            Statement statement = null;
+            ResultSet resultSet = null;
+            
+            Customer customer = new Customer();
+            
+            connection = DriverManager.getConnection(FULL_DB_URL, DB_USER, DB_PASSWD);
+            System.out.println("Is connection valid? " + connection.isValid(5));
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery("SELECT * FROM customers;");
+            resultSet.last();
+            System.out.println("Rows : " + resultSet.getRow());
+            resultSet.first();
+            System.out.println("Customer's Id: " + resultSet.getString(1) + 
+                               ", First Name: "  + resultSet.getString(2) + 
+                               ", Last Name: "   + resultSet.getString(3) + 
+                               ", Email: "       + resultSet.getString(4));
+            
+            
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(MainClass.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("No luck!");
+        }
     
     
     }
