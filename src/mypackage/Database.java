21d1300e3f13a52c12da77ed4c667c406920c3a4
David Oster <paspa@hotmail.com>
2020-04-06 14:28:13 +0300

    form for save/update

---
+++ src/mypackage/Database.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mypackage;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author mac
+ */
+public class Database {
+    private static final String DB_URL = "localhost:3306";
+    private static final String FULL_DB_URL = "jdbc:mysql://" + DB_URL + "/eshop?zeroDateTimeBehavior=CONVERT_TO_NULL&useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC&allowPublicKeyRetrieval=true&useSSL=false";
+    private static final String DB_USER = "root";
+    private static final String DB_PASSWD = "Root1234!";
+    private static Connection connection = null;
+    private static Statement statement = null;
+    
+    
+    public Database() {
+        getConnection();
+    }
+    
+    
+//    public static Connection getConnection2() throws SQLException {
+//        connection = DriverManager.getConnection(FULL_DB_URL, DB_USER, DB_PASSWD);
+//        return connection;
+//    }
+
+    public static Connection getConnection() {
+        try {
+            connection = DriverManager.getConnection(FULL_DB_URL, DB_USER, DB_PASSWD);
+            return connection;
+        } catch (SQLException ex) {
+            Logger.getLogger(Database.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+    }
+    
+    public static ResultSet getResults(String query) { // query = "SELECT * FROM Customers"
+        try {
+            setStatement();
+            ResultSet rs = statement.executeQuery(query);
+            return rs;
+        } catch (SQLException ex) {
+            Logger.getLogger(Database.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+    }
+
+    public static Statement getStatement() {
+        return statement;
+    }
+
+    public static void setStatement() {
+        try {
+            statement = connection.createStatement();
+        } catch (SQLException ex) {
+            Logger.getLogger(Database.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void setConnection(Connection connection) {
+        this.connection = connection;
+    }
+}
